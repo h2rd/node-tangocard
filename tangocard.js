b22e48d16ff5373e7b9d1528733d9dc98b2121e0
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2015-01-10 03:05:48 +0200

    initial

---
+++ tangocard.js
@@ -0,0 +1,82 @@
+var request = require('request')
+  , fs = require('fs');
+
+var certContent = fs.readFileSync('ca_cert/digicert_chain.pem');
+
+module.exports = function(options) {
+  var token = new Buffer(options.name + ':' + options.key).toString('base64')
+    , domain = options.domain || 'https://api.tangocard.com';
+
+  var _request = function(method, uri, payload, callback) {
+    if ('undefined' === typeof callback ) {
+      callback = payload;
+      payload = null;
+    }
+
+    method = method.toUpperCase() || 'GET';
+
+    var options = {
+      uri: domain + uri
+    , method: method
+    , qs: 'GET' === method ? payload : null
+    , headers: { 'Authorization': 'Basic ' + token }
+    , json: true
+    , body: 'GET' !== method ? payload : null
+    , agentOptions: { ca: certContent }
+    };
+
+    return request(options, function(err, req, body) {
+      if (err) {
+        return callback(err);
+      }
+
+      if (!body) {
+        return callback(new Error('API Response is empty'));
+      }
+
+      if (false === body.success && 'string' === typeof body.error_message) {
+        return callback(new Error(body.error_message));
+      }
+
+      return callback(null, body);
+    });
+  };
+
+  return {
+    createAccount: function(payload, callback) {
+      return _request('POST', '/raas/v1/accounts', payload, callback);
+    },
+
+    getAccountInfo: function(customer, accountId, callback) {
+      return _request('GET', '/raas/v1/accounts/' + customer + '/' + accountId, callback);
+    },
+
+    registerCreditCard: function(payload, callback) {
+      return _request('POST', '/raas/v1/cc_register', payload, callback);
+    },
+
+    fundAccount: function(payload, callback) {
+      return _request('POST', '/raas/v1/cc_fund', payload, callback);
+    },
+
+    deleteCreditCard: function(payload, callback) {
+      return _request('POST', '/raas/v1/cc_unregister', payload, callback);
+    },
+
+    getRewards: function(callback) {
+      return _request('GET', '/raas/v1/rewards', callback);
+    },
+
+    placeOrder: function(payload, callback) {
+      return _request('POST', '/raas/v1/orders', payload, callback);
+    },
+
+    getOrderInfo: function(orderId, callback) {
+      return _request('GET', '/raas/v1/orders/' + orderId, callback);
+    },
+
+    getOrderHistory: function(qs, callback) {
+      return _request('GET', '/raas/v1/orders', qs, callback);
+    }
+  }
+};
---
+++ test.js
@@ -0,0 +1,153 @@
+var expect = require('chai').expect
+  , crypto = require("crypto")
+  , Tango = require('./tangocard');
+
+var tangoClient = new Tango({
+  name: 'AiromoTest'
+, key: 'GfiK4xnQZD76AxX1xDJ01kkNOhtDMcptVMXQLjjBQe759NJsoZ7vYh2SY0'
+, domain: 'https://sandbox.tangocard.com'
+});
+
+var customer = randomString()
+  , accountIdentifier = randomString()
+  , cardToken = null;
+
+describe('Tango client', function() {
+  it('should initialize', function() {
+    expect(tangoClient).to.be.ok();
+    expect(tangoClient).to.have.property('createAccount');
+    expect(tangoClient).to.have.property('getAccountInfo');
+    expect(tangoClient).to.have.property('registerCreditCard');
+    expect(tangoClient).to.have.property('fundAccount');
+    expect(tangoClient).to.have.property('deleteCreditCard');
+    expect(tangoClient).to.have.property('getRewards');
+    expect(tangoClient).to.have.property('placeOrder');
+    expect(tangoClient).to.have.property('getOrderInfo');
+    expect(tangoClient).to.have.property('getOrderHistory');
+  })
+
+  it('should create an account', function(done) {
+    tangoClient.createAccount({
+      customer: customer
+    , identifier: accountIdentifier
+    , email: "test@test.com"
+    }, function(err, accountInfo) {
+      expect(accountInfo).to.be.ok;
+      expect(accountInfo.success).to.be.true;
+      expect(accountInfo.account.customer).to.be.equal(customer);
+      expect(accountInfo.account.identifier).to.be.equal(accountIdentifier);
+      expect(accountInfo.account.email).to.be.equal('test@test.com');
+      expect(accountInfo.account.available_balance).to.be.equal(0);
+      done(err);
+    });
+  });
+
+  it('should return account information', function(done) {
+    tangoClient.getAccountInfo(customer, accountIdentifier, function(err, accountInfo) {
+      expect(accountInfo).to.be.ok;
+      expect(accountInfo.success).to.be.true;
+      expect(accountInfo.account.customer).to.be.equal(customer);
+      expect(accountInfo.account.identifier).to.be.equal(accountIdentifier);
+      expect(accountInfo.account.email).to.be.equal('test@test.com');
+      expect(accountInfo.account.available_balance).to.be.equal(0);
+      done(err);
+    });
+  });
+
+  it('should register a credit card', function(done) {
+    var payload = {
+      customer: customer,
+      account_identifier: accountIdentifier,
+      client_ip: "127.0.0.1",
+      credit_card: {
+        number: "4111111111111111",
+        security_code: "123",
+        expiration: "2016-01",
+        billing_address: {
+          f_name: "FName",
+          l_name: "LName",
+          address: "Address",
+          city: "Seattle",
+          state: "WA",
+          zip: "98116",
+          country: "USA",
+          email: "test@example.com"
+        }
+      }
+    };
+
+    tangoClient.registerCreditCard(payload, function(err, cardInfo) {
+      expect(cardInfo).to.be.ok;
+      expect(cardInfo.success).to.be.true;
+      expect(cardInfo).to.have.property('cc_token');
+      expect(cardInfo).to.have.property('active_date');
+
+      cardToken = cardInfo.cc_token;
+      done(err);
+    });
+  });
+
+  it('should fund an account', function(done) {
+    // need to wait approval time
+    // we dont need to wait it
+    done();
+  });
+
+  it('should remove a credit card', function(done) {
+    var payload = {
+      customer: customer,
+      account_identifier: accountIdentifier,
+      cc_token: cardToken
+    };
+
+    tangoClient.deleteCreditCard(payload, function(err, cardInfo) {
+      expect(cardInfo).to.be.ok;
+      expect(cardInfo.success).to.be.true;
+      done(err);
+    });
+  });
+
+  it('should return list of rewards', function(done) {
+    tangoClient.getRewards(function(err, rewards) {
+      expect(rewards).to.be.ok;
+      expect(rewards.success).to.be.true;
+      expect(rewards).to.have.property('brands');
+      expect(rewards.brands).to.have.length.above(0);
+
+      done(err);
+    });
+  });
+
+  it('should place an order', function(done) {
+    // need to use credit card
+    done();
+  });
+
+  it('should return information about order', function(done) {
+    // need to make order
+    done();
+  });
+
+  it('should return order history', function(done) {
+    var qs = {
+      customer: customer
+    , account_identifier: accountIdentifier
+    };
+
+    tangoClient.getOrderHistory(qs, function(err, history) {
+      expect(history).to.be.ok;
+      expect(history.success).to.be.true;
+      expect(history).to.have.property('orders');
+      expect(history).to.have.property('offset');
+      expect(history).to.have.property('limit');
+      expect(history).to.have.property('result_count');
+      expect(history).to.have.property('total_count');
+      done();
+    });
+  });
+});
+
+
+function randomString() {
+  return crypto.randomBytes(20).toString('hex');
+}
